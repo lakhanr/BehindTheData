18e02984b25bb7489cbca994f0eb8040bdd82664
priteer <2015pritee.wadhwa@ves.ac.in>
2018-03-10 16:45:18 +0530

    Created courses,projects,contact us

---
+++ BeyondTheData/src/app/courses/courses.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListCoursesComponent } from './list-courses/list-courses.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ListCoursesComponent]
+})
+export class CoursesModule { }
---
+++ BeyondTheData/src/app/projects/projects.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListProjectsComponent } from './list-projects/list-projects.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ListProjectsComponent]
+})
+export class ProjectsModule { }
